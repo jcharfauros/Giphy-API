2b9b44907ec82970986c6d828e6442a45899b6c4
jcharfauros <julia.charfauros@gmail.com>
2021-07-14 01:01:44 -0400

    Updated code to pull from 2 endpoints, gifs and stickers. Updated styling.

--- script.js
+++ script.js
@@ -3,47 +3,73 @@ const randomURL = 'https://api.giphy.com/v1/gifs/random';
 const randomStickerURL = 'https://api.giphy.com/v1/stickers/random';
 const apiKey = 'jGkvhmeGrWFh4XDMnBzj5sjxbasRgVbo';
 
+const displayFetch = document.getElementById('display-fetch');
+const displayFetch2 = document.getElementById('display-fetch2');
+const displayInfo = document.getElementById('display-Info');
+const displayInfo2 = document.getElementById('display-Info2');
+
 const fetchBtn = document.getElementById('button-fetch');
-    fetchBtn.addEventListener('click', btnClick);
+fetchBtn.addEventListener('click', btnClick);
 
 function getGiphy() {
     const url = randomURL + '?api_key=' + apiKey + '&tag=' + 'kawaii';
-    // const url = `${randomURL}?api_key=${apiKey}`;
-    // const url = `${randomStickerURL}?api_key=${apiKey}`;
-
-    fetch(url)
-    .then(function(result) {
-        return result.json();
-    }) .then(function(json) {        
-        displayGiphy(json);            
-    });        
+    const stickUrl = randomStickerURL + '?api_key=' + apiKey + '&tag=' + 'kawaii'; 
+    
+    Promise.all([
+        fetch(url),
+        fetch(stickUrl)
+    ]) .then (function (result) {
+        return Promise.all(result.map(function (result){
+            return result.json();
+        }));
+    }) .then(function (json) {      
+            displayGiphy(json);
+    }); 
+    // .catch(function(error) {
+    //     console.log(error);
+    // });
 }
 getGiphy();
 
 function displayGiphy(giphy) {
-    console.log(giphy);
+    // console.log(giphy);
 
-    let displayFetch = document.getElementById('display-fetch');
-    displayFetch.innerText = ''; //clear previous
+     //clear previous
+    displayFetch.innerText = '';
+    displayFetch2.innerText = '';
+    displayInfo.innerText = '';
+    displayInfo2.innerText = '';    
         
     let giphyTitle = document.createElement('h2');
-    giphyTitle.innerText = giphy.data.title; 
+    giphyTitle.innerText = giphy[0].data.title; 
+    let giphyTitle2 = document.createElement('h2');
+    giphyTitle2.innerText = giphy[1].data.title; 
         
-    let giphyLink = document.createElement('a');
-    giphyLink.href = giphy.data.bitly_gif_url;
-    giphyLink.target = '_blank';
-    giphyLink.textContent = 'Add to favorites on Giphy';
-    // giphyLink.textContent = giphy.data.title;
-
-    let fetchImg = giphy.data.images.fixed_height.url;    
+    
+    let fetchImg = giphy[0].data.images.fixed_height.url;    
     let img = document.createElement('img');
-    img.setAttribute('src', fetchImg);    
-        
+    img.setAttribute('src', fetchImg);
+    
+    let fetchImg2 = giphy[1].data.images.fixed_height.url;    
+    let img2 = document.createElement('img');
+    img2.setAttribute('src', fetchImg2);
+    
+    let giphyLink = document.createElement('a');
+    giphyLink.href = giphy[0].data.bitly_gif_url;
+    giphyLink.target = '_blank';
+    giphyLink.textContent = 'Favorite';
+    
+    let giphyLink2 = document.createElement('a');
+    giphyLink2.href = giphy[1].data.bitly_gif_url;
+    giphyLink2.target = '_blank';
+    giphyLink2.textContent = 'Favorite';
+    
     displayFetch.appendChild(img);
-    displayFetch.appendChild(giphyTitle);
-    displayFetch.appendChild(giphyLink);
-    // displayFetch.appendChild(username);
-
+    displayFetch2.appendChild(img2);
+    displayInfo.appendChild(giphyTitle);
+    displayInfo2.appendChild(giphyTitle2);
+    displayInfo.appendChild(giphyLink);
+    displayInfo2.appendChild(giphyLink2);
     
 }
 function btnClick() {
